cb7e1ea4caaed11ac599ea8a0c3e90d6741720d2
Antara Chanda <160458895+meantaraa@users.noreply.github.com>
2025-09-05 20:59:57 +0530

    first commit

---
+++ Weather Observation Station 12.sql
@@ -0,0 +1,5 @@
+
+select distinct city from station
+where 
+not(city like 'A%' or city like 'E%' or city like 'I%' or city like 'O%' or city like 'U%') and
+not(city like '%A' or city like '%E' or city like '%I' or city like '%O' or city like '%U');
---
+++ Weather Observation Station 15.sql
@@ -0,0 +1,1 @@
+lat_n = (select max(lat_n) from station where lat_n<137.2345);
